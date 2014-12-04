8de728127af96bbf5991076267234480be7938d9
Louis <lhcoyle4@gmail.com>
2014-12-03 22:21:30 -0800

    Finished strcpy testbench
    Thank you so much for fixing the branching and LDR, Johnny. I was able
    to get my strcpy working as a result.

--- test2.vhd
+++ test2.vhd
@@ -27,10 +27,107 @@ architecture t2 of test2 is
   type register_file_t is array(0 to 15) of unsigned(31 downto 0);
   type imem_t is array(0 to 1000) of unsigned(15 downto 0);
   
+/* ASM file to be implemented in instruction memory
+  
+  MOV r0, #0   -- source address
+  MOV r1, #10  -- dest address
+  MOV r2, #0   -- to store each word of the string
+  MOV r3, #0   -- to save each word during byte testing
+  MOV r6, #0   -- to count the number of words copied
+  MOV r7, #0   -- for storing the number of bytes to rotate by
+
+copy:
+
+  MOV r4, #255  -- initialize register to and words with to get bytes
+  MOV r5, #0    -- and register to count number of bytes checked
+
+  LDR r2, [r0]    -- load and store 1 word
+  STR r2, [r1]
+
+  ADD r0, #1    -- increment source and dest addresses to next word
+  ADD r1, #1
+  ADD r6, #1    -- increment the number of words copied
+
+checknull:
+
+  MOV r3, r2  -- save loaded value
+  ROR r3, r7  -- rotate the word to check the next byte for null terminator
+  
+  AND r3, r4  -- select the rightmost byte
+
+  CMP r3, #0  -- check if it is the null terminator
+  BEQ verify  -- if so, end program
+
+  ADD r7, #8  -- increment the amount to rotate by a byte
+  ADD r5, #1  -- increment the counter for number of bytes checked
+
+  CMP r5, #4  -- if all bytes have been checked and there is no null
+  BEQ copy    -- then return to the start of the program (after initialization)
+  B checknull -- otherwise there are more bytes to check
+
+verify:
+  SUB r0, #1  -- decrement source and dest addresses to previous word
+  SUB r1, #1  -- they are at the last word because of previous instructions
+  
+  LDR r2, [r0]  -- load r2 with the source word
+  LDR r3, [r1]  -- and r3 with the dest word
+
+  SUB r6, #1  -- decrement the number of words that have been copied
+
+  CMP r2, r3  -- cmp the two
+  BNE fail    -- if they are not equivalent, the strcpy failed
+  CMP r6, #0  -- if there are no words left to copy, and strcpy didnt fail
+  BEQ success -- then report a success
+  B verify    -- otherwise there are still words to check
+
+
+fail:
+  MOV r0, #255 -- 0xFF is exit failure
+
+success:
+  MOV r0, #0  -- 0 is exit success
+  
+*/
+  
   signal imem: imem_t := (
-  0 => x"ABCD",
+  0 => x"2000", -- MOV r0, #0 (source contains "helloworld!")
+  1 => x"210A", -- MOV r1, #10 (dest)
+  2 => x"2200", -- MOV r2, #0
+  3 => x"2300", -- MOV r3, #0
+  4 => x"2600", -- MOV r6, #0
+  5 => x"2700", -- MOV r7, #0
+  6 => x"24FF", -- copy: MOV r4, #255
+  7 => x"2500", -- MOV r5, #0
+  8 => x"6802", -- LDR r2, [r0]
+  9 => x"600A", -- STR r2, [r1]
+  10 => x"3001", -- ADD r0, #1
+  11 => x"3101", -- ADD r1, #1
+  12 => x"3601", -- ADD r6, #1
+  13 => x"1C13", -- checknull: MOV r3, r2
+  14 => x"41FB", -- ROR r3, r7
+  15 => x"4023", -- AND r3, r4
+  16 => x"2B00", -- CMP r3, #0
+  17 => x"D004", -- BEQ verify
+  18 => x"3708", -- ADD r7, #8
+  19 => x"3501", -- ADD r5, #1
+  20 => x"2D04", -- CMP r5, #4
+  21 => x"D0F0", -- BEQ copy
+  22 => x"E7F5", -- B checknull
+  23 => x"1E40", -- verify: SUB r0, #1
+  24 => x"1E49", -- SUB r1, #1
+  25 => x"6802", -- LDR r2, [r0]
+  26 => x"680B", -- LDR r3, [r1]
+  27 => x"1E76", -- SUB r6, #1
+  28 => x"429A", -- CMP r2, r3
+  29 => x"D102", -- BNE fail
+  30 => x"2E00", -- CMP r6, #0
+  31 => x"D001", -- BEQ success
+  32 => x"E7F5", -- B verify
+  33 => x"20FF", -- fail: MOV r0, #255
+  34 => x"2000", -- success: MOV r0, #0
   others => x"0000"
   );
+
 
   signal clock: std_logic := '0';
   signal instruction: unsigned(15 downto 0) := "UUUUUUUUUUUUUUUU";
@@ -78,87 +175,8 @@ begin
     imem(2) <= "0110000000001001"; -- STR R1, [R1]
     imem(3) <= "1110011111111110"; -- B -4 -- skip back
     */
-    
-    imem(0) <= "0010000000000000"; -- MOV R0, #0 Load Address of "DigiPen" String to R0
-    -- Change imem(1) to n = 6E for Valid test
-    imem(1) <= "0010000101101110"; -- MOV R1, 'n' Fill Character 'n' in R1
-    imem(2) <= "0010001000000010"; -- MOV R2, #2 Load Address(2) into R2
-    imem(3) <= "0010010100000000"; -- MOV Counter(R5), #0
-    --Start: ;/*Find Character*/
-    imem(4) <= "0110100000000011"; -- LDR ExtractWord(R3), [StringData(R0)] Grab Data Location apply Offset
-    imem(5) <= "0001110000011111"; -- MOV R7, ExtractWord(R3); Move the Contents of ExtractWord to R7
-    --ByteReview: 
-    imem(6) <= "1011001011011100"; -- UXTB ByteCheck(R4), ExtractWord(R3);
-    imem(7) <= "0100001010001100"; -- CMP ByteCheck(R4), CharacterToTest(R1); Compare it with Data
-    imem(8) <= "1101000000001000"; -- BEQ ExitBlock;
-    imem(9) <= "0010110000000000"; -- CMP ByteCheck(R4), #0; Check Null
-    imem(10)<= "1101000000001000"; -- BEQ FoundNothing; Found a null exit 
-    imem(11)<= "0001110001101101"; -- ADD Counter(R5), Counter(R5), #1; Move the Counter up by 1
-    imem(12)<= "0010011000001000"; -- MOV R6, #8;
-    imem(13)<= "0100000111110011"; -- ROR ExtractWord(R3), R6 ; Remove the LSBytes
-    imem(14)<= "0100001010111011"; -- CMP ExtractWord(R3), R7; Check with Rotated Content
-    imem(15)<= "1101000111110101"; -- BNE ByteReview;
-    imem(16)<= "0001110001000000"; -- ADD StringData(R0), StringData(R0), #1; // Changed Shift by 4(0001110100000000)
-    imem(17)<= "1110011111110001"; -- B Start; Start over
-    --ExitBlock:
-    imem(18)<= "0110000000010101"; -- STR Counter(R5), [ReturnLocation(R2)];
-    imem(19)<= "1110000000000010"; -- B EndStuff; Finished Right
-    --FoundNothing:
-    imem(20)<= "0010010100000000"; -- MOV R5, #0;
-    imem(21)<= "0001111001101101"; -- SUB R5, R5, #1; Stupid way of getting -1
-    imem(22)<= "0110000000010101"; -- STR R5, [ReturnLocation(R2)];
-    --EndStuff
-    -- Change imem(1) to @ = 40 for -1 test
-    imem(23) <= "0010000000000000"; -- MOV R0, #0 Load Address of "DigiPen" String to R0
-    imem(24) <= "0010000101000000"; -- MOV R1, 'n' Fill Character '40' in R1
-    imem(25) <= "0010010100000000"; -- MOV Counter(R5), #0
-    imem(26) <= "1110011111110101"; -- B Start; Start over
-    wait for 80 ns; -- Instruction 0 and 1 pass
-    assert reg(1) = 110 report "Failed to Fill R1 with 'n'"; 
-    wait for 20 ns; -- Instruction 2
-    assert reg(2) = 2 report "Failed to Fill R2 with 2";
-    wait for 20 ns; -- Instruction 3
-    assert reg(5) = 0 report "Failed to Clear the Counter";
-    wait for 20 ns; -- Instruction 4 LDR NEEDS TO CHANGE AND TIMING NEEDS TO CHANGE AS WELL
-    wait for 40 ns; -- Instruction 4 hold
-    assert reg(3) = x"69676944" report "Failed to Grab first byte";
-    wait for 20 ns; -- Instruction 5
-    assert reg(7) = x"69676944" report "Failed to Move Extracted Word";
-    -- Loop starts here
-    wait for 20 ns; -- Instruction 6
-    assert reg(4) = x"44" report "Failed to Extract First Byte"; -- Letter D
-    wait for 20 ns; -- Instruction 7 CMP First 
-    wait for 20 ns; -- Instruction 8 Skip Branch First Time
-    wait for 20 ns; -- Instruction 9 CMP Second
-    wait for 20 ns; -- Instruction 10 Skip Branch
-    wait for 20 ns; -- Instruction 11 
-    assert reg(5) = 1 report "Did not add counter First Character";
-    wait for 20 ns; -- Instruction 12
-    assert reg(6) = 8 report "Did not Rotate Move";
-    wait for 20 ns; -- Instruction 13 
-    assert reg(3) = x"44696769" report "Failed to Shift";
-    wait for 20 ns; -- Instruction 14
-    wait for 60 ns; -- Instruction 15 Branch should happen (need to change time)
-    wait for 180 ns; -- Skip through Instruction 6-15 (Letter i)
-    wait for 60 ns; -- Instruction 15 Branch should happen (need to change time)
-    wait for 180 ns; -- Skip through Instruction 6-15 (Letter g)
-    wait for 60 ns; -- Instruction 15 Branch should happen (need to change time)
-    wait for 180 ns; -- Skip through Instruction 6-15 (Letter i)
-    wait for 20 ns; -- Branch should NOT happen
-    wait for 20 ns; -- Instruction 16
-    assert reg(0) = 1 report "Address was shifted by one";
-    wait for 60 ns; -- Branch to start (need to change branch time)
-    wait for 80 ns; -- Instruction 4 and 5
-    wait for 180 ns; -- Skip through Instruction 6-15 (Letter p)
-    wait for 60 ns; -- Instruction 15 Branch should happen (need to change time)
-    wait for 160 ns; -- Skip through Instruction 6-15 (Letter e)
-    wait for 60 ns; -- Instruction 15 Branch should happen (need to change time)
-    wait for 120 ns; -- Skip through Instruction Success (Letter n)
-    wait for 60 ns; -- Instruction 8 Branch should happen (need to change time)
-    wait for 40 ns; -- Instruction 18 and 19
-    assert reg(5) = 6 report "Character Find Failed to Happen";
-    wait for 2240 ns; -- Skip through 
-    assert reg(5) = x"FFFFFFFF" report "Miss character"; -- A certain amount of time later the project is complete
+    wait for 3660 ns; -- wait some long period of time until it is done copying and verifying
+    assert reg(0) = 0 report "strcpy failed, program reported exit failure";
     report "Simulation complete";
     wait;
   end process;
